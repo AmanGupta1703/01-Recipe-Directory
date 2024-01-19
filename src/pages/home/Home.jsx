9a33c37d4260cda88fa8e9a40ca4812d5c6a76d0
AmanGupta1703 <guptakunalx@gmail.com>
2024-01-19 10:12:55 +0530

    Fetch recipes in Home component

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,8 +1,19 @@
+// custom hook
+import { useFetch } from "../../hooks/useFetch";
+
 // styles
 import "./Home.css";
 
 function Home() {
-	return <div>Home</div>;
+	const { data, isPending, error } = useFetch("http://localhost:3000/recipes");
+
+	return (
+		<div className="home">
+			{error && <p className="error">{error}</p>}
+			{isPending && <p className="loading">Loading...</p>}
+			{data && data.map((recipe) => <div key={data.id}>{recipe.title}</div>)}
+		</div>
+	);
 }
 
 export default Home;
