0b3a6b1ffeff579de10fd54439ddd1db56c55bab
AmanGupta1703 <guptakunalx@gmail.com>
2024-01-19 08:55:42 +0530

    Add react-router-dom dependency create home, create, search, recipe pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import { Home, Create, Recipe, Search } from "./pages";
+
+// styles
 import "./App.css";
 
 function App() {
-	return <div className="App"></div>;
+	return (
+		<div className="App">
+			<BrowserRouter>
+				<Routes>
+					<Route index path="/" element={<Home />} />
+					<Route path="/create" element={<Create />} />
+					<Route path="/search" element={<Search />} />
+					<Route path="/recipe/:id" element={<Recipe />} />
+				</Routes>
+			</BrowserRouter>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/pages/index.js
@@ -0,0 +1,6 @@
+import Home from "./home/Home.jsx";
+import Search from "./search/Search.jsx";
+import Recipe from "./recipe/Recipe.jsx";
+import Create from "./create/Create.jsx";
+
+export { Home, Search, Recipe, Create };
