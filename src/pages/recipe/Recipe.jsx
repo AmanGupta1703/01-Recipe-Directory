f890065537ebd7380272ec885386402a352b2430
AmanGupta1703 <guptakunalx@gmail.com>
2024-01-23 10:33:46 +0530

    Add recipe details to Recipe component

--- src/pages/recipe/Recipe.jsx
+++ src/pages/recipe/Recipe.jsx
@@ -1,8 +1,42 @@
+import { useParams } from "react-router-dom";
+
+// custom hook
+import { useFetch } from "../../hooks/useFetch";
+
 // styles
 import "./Recipe.css";
 
 function Recipe() {
-	return <div>Recipe</div>;
+	const { id } = useParams();
+
+	const {
+		data: recipe,
+		error,
+		isPending,
+	} = useFetch(`http://localhost:3000/recipes/${id}`);
+
+	if (error) {
+		return <p className="error">{error}</p>;
+	}
+
+	if (isPending) {
+		return <p className="loading">Loading...</p>;
+	}
+
+	if (recipe) {
+		return (
+			<div className="recipe">
+				<h2 className="page-title">{recipe.title}</h2>
+				<p>Takes {recipe.cookingTime} to cook.</p>
+				<ul>
+					{recipe.ingredients.map((ing) => (
+						<li key={ing}>ing</li>
+					))}
+				</ul>
+				<p className="method">{recipe.method}</p>
+			</div>
+		);
+	}
 }
 
 export default Recipe;
